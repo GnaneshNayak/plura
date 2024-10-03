2ee90fd90cd008e0ddc11231b1835cd8e5a1a2ce
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2024-10-04 00:09:25 +0530

    added subaccount  loading page

---
+++ src/app/(main)/subaccount/dist/loading.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var Loading_1 = require("@/components/global/Loading");
+var react_1 = require("react");
+var LoadingAgencyPage = function () {
+    return (react_1["default"].createElement("div", { className: "h-screen w-screen flex justify-center items-center" },
+        react_1["default"].createElement(Loading_1["default"], null)));
+};
+exports["default"] = LoadingAgencyPage;
